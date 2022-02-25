5a186b2064e04452087291402ae5fe7a81d53049
Osnago-L <osna.brera@gmail.com>
2022-02-25 13:15:08 +0100

    class end

--- classes/Movies.php
+++ classes/Movies.php
@@ -1,5 +1,20 @@
 <?php
 class Movies
 {
-    
+    private $title;
+    private $genre;
+    private $year;
+    private $rating;
+
+    public function __construct($_title,$_genre,$_year){
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+    }
+
+    public function setRating($_rating){
+        if (($_rating>=0 && $_rating<=5)&&(is_numeric($_rating))) {
+            $this->rating = $_rating;
+        } 
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once __DIR__."./classes/Movies.php";
+
+$film1 = new Movies();
+$film2 = new Movies();
+
+var_dump($film1);
